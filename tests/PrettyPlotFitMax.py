b42cbe0a03a4fb64e7c0af033fcc2cb3b83c71ba
Simone Ponzio <simone.ponzio.94@gmail.com>
2019-11-09 18:07:18 +0100

    restored and improved test for fitness max

--- tests/PrettyPlotFitMax.py
+++ tests/PrettyPlotFitMax.py
@@ -2,51 +2,50 @@
 
 import random
 from deap import creator, base, tools, algorithms
-from utilities import BarBinFct, BarBinSeq, GenBinSeq
+from utilities import PlotBinSeq, ScatBinFct, GenBinSeq
 import matplotlib.pyplot as plt
-from NitchingMethods import FitSharing
-from distFunctions import NormHamming2
-from FitFunctions import MaxMinEval
+from FitFunctions import MaxMinEval, FnctA, FnctB
 
 # define Individual size
-IND_SIZE = 10
+IND_SIZE = 12
 # define num of individual
-NUM_IND = 200
+NUM_IND = 100
 # define num of generations
-NUM_GEN = 3
+NUM_GEN = 50
+# define the fitness function between [FnctA, FnctB]
+FitnessFunction = FnctA
 
-creator.create("FitnShare", base.Fitness, weights=(1.0,))
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("Individual", list, fitness=creator.FitnessMax, fitshare=creator.FitnShare)
+creator.create("Individual", list, fitness=creator.FitnessMax)
 
 toolbox = base.Toolbox()
 
-toolbox.register("evalfit", MaxMinEval)
-toolbox.register("evalfitsh", FitSharing, fitFunction=toolbox.evalfit, distanceFunction=NormHamming2)
 toolbox.register("attr_bool", random.randint, 0, 1)
 toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, n=IND_SIZE)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
+toolbox.register("evaluate", FitnessFunction)
 toolbox.register("mate", tools.cxTwoPoint)
 toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
-toolbox.register("select", tools.selTournament, tournsize=3, fit_attr='fitshare')
+toolbox.register("select", tools.selTournament, tournsize=3)
 
 population = toolbox.population(n=NUM_IND)
 
 AllBinSeq = GenBinSeq(IND_SIZE)
-AllPossibleFits = [MaxMinEval(i)[0] for i in AllBinSeq]
+AllPossibleFits = [FitnessFunction(i)[0] for i in AllBinSeq]
 
 for gen in range(NUM_GEN):
     offspring = algorithms.varAnd(population, toolbox, cxpb=0.5, mutpb=0.1)
-    for ind in offspring:
-        ind.fitshare.values = toolbox.evalfitsh(ind, offspring)
+    fits = toolbox.map(toolbox.evaluate, offspring)
+    for fit, ind in zip(fits, offspring):
+        ind.fitness.values = fit
     population = toolbox.select(offspring, k=len(population))
 
-    BarBinSeq(AllBinSeq, AllPossibleFits)
-    BarBinSeq(population, [MaxMinEval(i)[0] for i in population])
-    plt.show()
+PlotBinSeq(AllBinSeq, AllPossibleFits, 'r')
+ScatBinFct(population, FitnessFunction)
+plt.show()
 
 for ind in population:
-    ind.fitness.values = toolbox.evalfit(ind)
+    ind.fitness.values = toolbox.evaluate(ind)
 top3 = tools.selBest(population, k=3, fit_attr='fitness')
 print(top3)
